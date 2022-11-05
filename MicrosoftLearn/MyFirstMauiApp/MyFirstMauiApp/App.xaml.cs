6beaa2732ef02f4b9f739e9045c163735e15b9f6
marcos.henrique <marcos.henrique@engegraph.com.br>
2022-11-05 15:42:58 -0300

    Initial commit

---
+++ MicrosoftLearn/MyFirstMauiApp/MyFirstMauiApp/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace MyFirstMauiApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+
+    // Manipuladores comum de eventos do app
+    protected override void OnStart()
+    {
+		base.OnStart();
+    }
+
+    protected override void OnResume()
+    {
+        base.OnResume();
+    }
+
+    protected override void OnSleep()
+    {
+        base.OnSleep();
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MAUI
+ Aulas, desafios e projetos feitos para aprender sobre a técnologia MAUI do DotNet/C#
