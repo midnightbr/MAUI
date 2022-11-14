af6ad9878abbf790d8c0ae7e2c8ad2131de381bf
marcos.henrique <marcos.henrique@engegraph.com.br>
2022-11-14 07:47:34 -0300

    - Alterado dependências e debug - Criado uma tela de Login demonstrado no curso

---
+++ MicrosoftLearn/MyFirstMauiApp/MyFirstMauiApp/Login.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace MyFirstMauiApp;
+
+public partial class Login : ContentPage
+{
+    public Login()
+    {
+        InitializeComponent();
+        LoginIn.Clicked += LoginButton_Clicked;
+    }
+
+    private string userName;
+    private string password;
+
+    private void LoginButton_Clicked(object sender, EventArgs e)
+    {
+        Debug.WriteLine("Clicked!");
+    }
+}
