38890d7eb32ed7a698fee0956553d8ae3daeeaaa
marcos.henrique <marcos.henrique@engegraph.com.br>
2022-11-20 16:33:21 -0300

    - Atualizado as dependencias do projeto em relação a versão do dotnet - Aprendido e utilizado sobre as sepração de preocupações - Aprendindo sobre a Classe Extensão de Marcação - Criado uma tela de login simples (sem funcionalidades ainda)

--- MicrosoftLearn/MyFirstMauiApp/MyFirstMauiApp/Login.xaml.cs
+++ MicrosoftLearn/MyFirstMauiApp/MyFirstMauiApp/Login.xaml.cs
@@ -4,6 +4,8 @@ namespace MyFirstMauiApp;
 
 public partial class Login : ContentPage
 {
+    public const double MyFontSize = 20;
+
     public Login()
     {
         InitializeComponent();
@@ -16,5 +18,20 @@ public partial class Login : ContentPage
     private void LoginButton_Clicked(object sender, EventArgs e)
     {
         Debug.WriteLine("Clicked!");
+    }
+}
+
+
+/**
+ * O campoMyFontSizedeve ser um membro da classeMainPagepara permitir que ele seja referenciado no método ProvideValue à sua maneira.
+ * As boas práticas implicam que, neste caso, a variável também deve ser uma constante. O valor é.static, const ou static.
+ * O método ProvideValue também pode fazer ajustes no valor retornado, dependendo da orientação e do fator de forma do dispositivo.
+ */
+// Criando uma extensão de marcação
+public class GlobalFontSizeExtension : IMarkupExtension
+{
+    public object ProvideValue(IServiceProvider serviceProvider)
+    {
+        return Login.MyFontSize;
     }
 }
